ecaaa9cf56f6b9a16856546d314ba0e4a2f68c11
Jon Connolly <jontconnolly@gmail.com>
2019-05-06 15:09:44 -0700

    add setup and modify travis

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
-"""run pip install -e .
- in root directory to let python know where to find package
-"""
-from setuptools import setup
-setup(name='app')
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="MagD",
+    version="1.0.",
+    author="Jon Connolly",
+    author_email="joncon@uw.edu",
+    description="A simple API to return GeoJSON tremor data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnsn/tremor_api",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
